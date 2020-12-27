bff6b7635c2beeae9a3ca95a0a76cf5762adfc55
Artyom Vyatkin <vyatkin.artex@gmail.com>
2020-12-28 00:20:37 +0300

    fixed some minor issues

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 from datetime import datetime, timedelta
 
 from vk_bot import VkBot
-import logging
 import threading
 import schedule
 import time
 
-
-def vk_start(vk_bot1):
-    logging.basicConfig(format='%(asctime)s %(name)s - %(levelname)s:%(message)s', level=logging.INFO)
-    logging.info("Start")
-    vk_bot1.run()
+vk_bot = VkBot()
 
 
-def send_news(vk_bot1):
+def vk_start():
+    vk_bot.run()
+
+
+def send_news():
     current_datetime = datetime.today() + timedelta(hours=1)
     possible_sending_time = [
         current_datetime.strftime('M-%d-%H'),
@@ -21,28 +20,20 @@ def send_news(vk_bot1):
         current_datetime.strftime('%H')
     ]
 
-    users = vk_bot1.db.get_subscribed_users_to_send_news(possible_sending_time, current_datetime.date())
+    users = vk_bot.db.get_subscribed_users_to_send_news(possible_sending_time, current_datetime.date())
     for user in users:
-        news = vk_bot1.db.get_news(user.universities, user.last_sending_date, user.topics)
+        news = vk_bot.db.get_news(user.universities, user.last_sending_date, user.topics)
         vk_bot.send_news_out_of_state_handling(user.vk_id, news)
 
 
-def job3(vk_bot1):
-    print("3 --- FROM SCHEDULE -- I'm running on thread %s" % threading.current_thread())
-
-
-def start_schedule1(vk_bot1):
-    schedule.every().minute.at(":00").do(send_news, vk_bot1)
-    schedule.every().minute.at(":30").do(send_news, vk_bot1)
+def start_schedule(vk_bot1):
+    schedule.every().hour.at(":00").do(send_news, vk_bot1)
     while 1:
-        print("!!!!")
         schedule.run_pending()
-        time.sleep(5)
+        time.sleep(10000)
 
 
-vk_bot = VkBot()
-
-thread_for_schedule = threading.Thread(target=start_schedule1, args=(vk_bot,))
+thread_for_schedule = threading.Thread(target=start_schedule, args=(vk_bot,))
 thread_for_schedule.start()
 
 thread_for_bot = threading.Thread(target=vk_start, args=(vk_bot,))
